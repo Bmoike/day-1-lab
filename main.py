b44149b1cbc63a7e13f32801fce5c25d3ead0ecb
Michael Kerr <kerrjaxm@gmail.com>
2024-08-07 21:47:06 -0400

    Initial Lab 1

---
+++ main.py
@@ -0,0 +1,26 @@
+# exercise 1: Echo String
+echo_input = input("Please enter your favorite color: ")
+print(echo_input)
+
+# exercise 2: Adding a number to an integer
+user_num = int(input("Please enter a number: "))
+print(user_num + 1)
+
+# exercise 3: Adding a number to a float
+new_num = int(input("Please enter a new number: "))
+print(new_num + .5)
+
+# exercise 4: Adding two floats
+num1 = float(input("Please enter a number with a decimal: "))
+num2 = float(input("Please enter second number with a decimal: "))
+print(f"The sum is {num1 + num2}")
+
+# exercise 5: Multiplying Floats
+deci1 = float(input("Please enter a float: "))
+deci2 = float(input("Please enter another float: "))
+print(f"The product is {deci1 * deci2}")
+
+# exercise 6: Dividing integers
+integer1 = int(input("Please enter an integer: "))
+integer2 = int(input("Please enter a second integer: "))
+print(f"The result is {integer1 / integer2}")
